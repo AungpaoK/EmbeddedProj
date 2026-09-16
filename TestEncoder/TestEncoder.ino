daec0077b9e6f3e600cad9ed3011642faa941e07
Phattharin K. <64649279+AungpaoK@users.noreply.github.com>
2026-09-16 20:33:48 +0700

    added a robotconfig library

---
+++ TestEncoder/TestEncoder.ino
@@ -0,0 +1,71 @@
+#include "robotconfig.h"
+
+uint8_t debugPrintIntervalMS = 20;
+unsigned long lastDebugPrintMS = 0;
+
+volatile long rightEncoderTicks = 0;
+volatile long leftEncoderTicks = 0;
+
+// Interrupt Service Routine for PORTC
+ISR(PCINT1_vect) {
+  static uint8_t lastPortC = 0;
+  uint8_t currentPortC = PINC; // Read Port C input register
+
+  // Detect rising edge on Phase A (A4 / PC4)
+  if ((currentPortC & (1 << PC4)) && !(lastPortC & (1 << PC4))) {
+    // Check Phase B to determine rotation direction
+    if (currentPortC & (1 << PC5)) {
+      leftEncoderTicks++;  // Forward
+    } else {
+      leftEncoderTicks--;  // Reverse
+    }
+  }
+
+  // Detect rising edge on Phase A (A4 / PC4)
+  if ((currentPortC & (1 << PC0)) && !(lastPortC & (1 << PC0))) {
+    // Check Phase B to determine rotation direction
+    if (currentPortC & (1 << PC1)) {
+      rightEncoderTicks--;  // Forward
+    } else {
+      rightEncoderTicks++;  // Reverse
+    }
+  }
+  lastPortC = currentPortC;
+}
+
+void setupEncoders() {
+  DDRC &= ~0b00110011;
+  PORTC |= (1 << PC0) | (1 << PC1) | (1 << PC4) | (1 << PC5);
+  PCICR |= (1 << PCIE1);
+  PCMSK1 |= (1 << PCINT9) | (1 << PCINT13);
+}
+
+void setup()
+{
+  Serial.begin(115200);
+
+  // Initialize L298N pins as OUTPUTs
+  DDRB |= 0b00111111;
+  
+  setupEncoders();
+  sei();
+}
+
+void loop()
+{
+  unsigned long now = millis();
+
+  if (now - lastDebugPrintMS >= debugPrintIntervalMS) {
+    lastDebugPrintMS = now;
+
+    cli();
+    long currentLeftTicks = leftEncoderTicks;
+    long currentRightTicks = rightEncoderTicks;
+    sei();
+
+    Serial.print(currentLeftTicks);
+    Serial.print(", ");
+    Serial.println(currentRightTicks);
+
+  }
+}
---
+++ robotconfig/robotconfig.h
@@ -0,0 +1,27 @@
+// MOVE THIS FILE INTO YOUR LIB FOR EASY USE
+// L298 pins
+
+#define IN4 7
+#define IN1 8
+#define IN2 9
+#define ENA 10
+#define ENB 11
+#define IN3 12
+
+// Right Encoder Pins
+
+#define RIGHT_ENC_A A1
+#define RIGHT_ENC_B A0
+
+// Left Encoder Pins
+
+#define LEFT_ENC_A A5
+#define LEFT_ENC_B A4
+
+// Wheel size
+
+#define WHEEL_RADIUS 0.065 //  6.5 cm
+#define WHEEL_BASE 0.343   // 34.3 cm
+
+// Motor Variable
+#define TICKS_PER_REV 1920.0
